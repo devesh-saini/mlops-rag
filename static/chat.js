b911a9fdf337a1dc4492fd1a1b5818b18e31fa77
Devesh Saini <deveshsaini369@gmail.com>
2025-11-04 18:14:50 +0530

    Initial styling done.

--- static/chat.js
+++ static/chat.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     let submit = document.getElementById("submit-button");
     submit.addEventListener("click", expand);
+    document.addEventListener("keydown", (event) => {
+        if (event.key === "Enter") {
+            expand();
+        }
+    })
 })
 
 function expand() {
@@ -11,4 +16,17 @@ function expand() {
 
     const avatar = document.querySelector(".avatar");
     avatar.style.width = "120px";
+
+    const chat_area = document.querySelector(".chat-area");
+    chat_area.style.borderRadius = "24px";
+    chat_area.style.minHeight = "56vh";
+    chat_area.style.minWidth = "88vw";
+    chat_area.style.maxWidth = "800px";
+    chat_area.style.margin = "16px";
+    chat_area.style.boxShadow = "0 5px 15px rgba(0, 0, 0, 0.07)";
+
+    const chat_bar_text = document.querySelector(".chat-bar-text");
+    user_query = chat_bar_text.value;
+    console.log(user_query);
+    chat_bar_text.value = "";
 }
